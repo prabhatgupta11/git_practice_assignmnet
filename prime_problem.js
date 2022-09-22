fcc91580732e5b65c640de79a00ed0c1a8ecfba0
Prabhat kumar gupta <guptajim3636@gmail.com>
2022-09-22 19:37:38 +0530

    coding of prime is done

---
+++ prime_problem.js
@@ -0,0 +1,17 @@
+let num=13;
+let temp=0;
+for(i=2;i<=num-1;i++)
+{
+  if(num%i==0)
+    { 
+       temp;
+    }
+}
+ if(temp==0)
+  { 
+     console.log("num; is prime number");
+  }
+  else
+  {
+    console.log("num; is not");  
+  }
